0fc3fe80ce9e73bca0c071f216043719d6ea4d5b
Beepbop94 <matthewpenn94@yahoo.co.uk>
2024-03-25 11:57:03 +0000

    updating the sql table reference

--- Chapter 2/Using Data Types/SelectTop10Customers.sql
+++ Chapter 2/Using Data Types/SelectTop10Customers.sql
@@ -1,11 +1,11 @@
 SELECT Customer.CustomerID,
-  FirstName,
-  LastName,
-  Sum(TotalDue)
+  Customer.FirstName,
+  Customer.LastName,
+  Sum(Orders.TotalDue)
 From Customer
   JOIN Orders on Customer.CustomerID = Orders.CustomerID
 GROUP BY Customer.CustomerID,
-  FirstName,
-  LastName
+  Customer.FirstName,
+  Customer.LastName
 ORDER BY Sum(TotalDue) DESC
 LIMIT 10 -- Click on Run on active connection to run
