75b265333f6eaed081dc495fd9a8ee18847d1b58
Beepbop94 <matthewpenn94@yahoo.co.uk>
2024-03-25 13:58:45 +0000

    notes

--- Chapter 2/Finding Inaccurate Data Values/CheckOrderTotal.sql
+++ Chapter 2/Finding Inaccurate Data Values/CheckOrderTotal.sql
@@ -1,8 +1,11 @@
 SELECT OrderItem.OrderId,
   Orders.TotalDue,
-  SUM(Price * Quantity) as NewTotalDue
+  Round(SUM(Product.Price * OrderItem.Quantity),2) as NewTotalDue
 FROM OrderItem
   JOIN Product ON OrderItem.ProductID = Product.ProductID
   JOIN Orders ON OrderItem.OrderID = Orders.OrderID
 GROUP BY OrderItem.OrderId,
   Orders.TotalDue
+Having Orders.TotalDue = NewTotalDue
+
+-- Checking the 2 values are equal
